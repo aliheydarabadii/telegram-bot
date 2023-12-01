c9a6d7c93fc74231a7914011b46883477dc61104
aliheydarabadii <aliheydarabadii@gmail.com>
2023-12-01 12:54:00 +0330

    add start handler

--- bot.py
+++ bot.py
@@ -19,6 +19,11 @@ async def start(update:Update, context:ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text('Please choose:', reply_markup=reply_markup)
 
 
+async def movie(update:Update, context:ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text(f'Hello {update.effective_user.first_name} {update.effective_user.id}')
+
+
+
 app = ApplicationBuilder().token("2019821043:AAEgjiZnRAnUWerheUfGglcAIwM6NVINcsQ").build()
 
 app.add_handler(CommandHandler("hello", hello))
@@ -27,5 +32,6 @@ app.add_handler(CommandHandler("hello", hello))
 
 app.add_handler(CommandHandler('start', start))
 
+app.add_handler(CommandHandler('فیلم', movie))
 
 app.run_polling()
