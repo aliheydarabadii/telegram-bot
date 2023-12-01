a249eb4ed47c2878d6800f0e840eb4c13023a10e
aliheydarabadii <aliheydarabadii@gmail.com>
2023-12-01 12:35:37 +0330

    add id

--- bot.py
+++ bot.py
@@ -5,7 +5,7 @@ from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 print("hello")
 
 async def hello(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    await update.message.reply_text(f'Hello {update.effective_user.first_name}')
+    await update.message.reply_text(f'Hello {update.effective_user.first_name} {update.effective_user.id}')
 
 
 app = ApplicationBuilder().token("2019821043:AAEgjiZnRAnUWerheUfGglcAIwM6NVINcsQ").build()
