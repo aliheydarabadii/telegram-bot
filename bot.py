4c2a0b9ea9d92f1f7e950de96d552ce4af8c94fe
aliheydarabadii <aliheydarabadii@gmail.com>
2023-12-01 12:32:26 +0330

    first commit

---
+++ bot.py
@@ -0,0 +1,15 @@
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+
+
+print("hello")
+
+async def hello(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    await update.message.reply_text(f'Hello {update.effective_user.first_name}')
+
+
+app = ApplicationBuilder().token("2019821043:AAEgjiZnRAnUWerheUfGglcAIwM6NVINcsQ").build()
+
+app.add_handler(CommandHandler("hello", hello))
+
+app.run_polling()
