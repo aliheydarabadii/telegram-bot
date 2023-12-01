4f0d28a680f96c48e5c7fa1f54fb655681421542
aliheydarabadii <aliheydarabadii@gmail.com>
2023-12-01 12:40:57 +0330

    add start handler

--- bot.py
+++ bot.py
@@ -1,6 +1,8 @@
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
+from telegram import ReplyKeyboardMarkup
+from telegram.ext import Updater
 
 print("hello")
 
@@ -8,8 +10,22 @@ async def hello(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     await update.message.reply_text(f'Hello {update.effective_user.first_name} {update.effective_user.id}')
 
 
+def start(update, context):
+    keyboard = [['Option 1', 'Option 2'],
+                ['Option 3', 'Option 4']]
+
+    reply_markup = ReplyKeyboardMarkup(keyboard, one_time_keyboard=True, resize_keyboard=True)
+
+    update.message.reply_text('Please choose:', reply_markup=reply_markup)
+
+
 app = ApplicationBuilder().token("2019821043:AAEgjiZnRAnUWerheUfGglcAIwM6NVINcsQ").build()
 
 app.add_handler(CommandHandler("hello", hello))
+
+
+
+app.add_handler(CommandHandler('start', start))
+
 
 app.run_polling()
