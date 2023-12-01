299a83c3673375e2129d5a34eb6b30f5ffe3f517
aliheydarabadii <aliheydarabadii@gmail.com>
2023-12-01 13:02:00 +0330

    add start handler

--- bot.py
+++ bot.py
@@ -22,9 +22,19 @@ async def start(update:Update, context:ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text('Please choose:', reply_markup=reply_markup)
 
 
-async def movie(update:Update, context:ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text(f'Hello {update.effective_user.first_name} {update.effective_user.id}')
+def button(update:Update, context):
+    query = update.callback_query
+    query.answer()  # This is necessary to prevent small loading circle on the button
 
+    # Handle the callback_data
+    if query.data == '1':
+        response = f"You selected Option 1.{update.effective_user.id}"
+    elif query.data == '2':
+        response = f"You selected Option 2.{update.effective_user.id}"
+    else:
+        response = f"Unknown option. {update.effective_user.id}"
+
+    query.edit_message_text(text=response)
 
 
 app = ApplicationBuilder().token("2019821043:AAEgjiZnRAnUWerheUfGglcAIwM6NVINcsQ").build()
@@ -35,6 +45,6 @@ app.add_handler(CommandHandler("hello", hello))
 
 app.add_handler(CommandHandler('start', start))
 
-app.add_handler(CommandHandler('1', movie))
+app.add_handler(CallbackQueryHandler(button))
 
 app.run_polling()
